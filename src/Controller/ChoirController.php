946170d5c15c88c11fe1470bbe722435bb9142a2
Yvan Emiliau <yvanemiliau>
2023-10-31 17:18:52 +0300

    Use annotation instead of routes.yaml

--- src/Controller/ChoirController.php
+++ src/Controller/ChoirController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ChoirController {
 
+    /**
+     * @Route("/choir", name="choir_homepage")
+     */
     public function index()
     {
         return new Response("This is the choir homepage");
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController {
 
+    /**
+     * @Route("/", name="homepage")
+     */
     public function index()
     {
         return new Response("This is my home page");
